82941c7c676b3802e581af65d0fdb5dd87a40508
ych <qq809638233@gmail.com>
2023-02-13 16:29:26 +0800

    updata 2.0 by surper

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/olekukonko/tablewriter"
 	log "github.com/sirupsen/logrus"
 	"io"
+	"math"
 	"os"
 	"strconv"
 	"time"
@@ -21,9 +22,10 @@ const (
 var (
 	//Address 开始地址位 Quantity 读取寄存器数量
 	Address  = flag.Int("a", 0, "Address")
-	Quantity = flag.Int("q", 1, "Quantity")
+	Quantity = flag.Int("q", 8, "Quantity")
 	// SerialNamePtr 串口号 BaudPtr 波特率 ReadTimeoutPtr 读取时间
 	ModPtr         = flag.String("m", "RTU", "MOD (TCP/RTU)")
+	DataTypePtr    = flag.String("dt", "uint16", "uint16, int16, uint32, int32, float32, float64...")
 	SerialNamePtr  = flag.String("sn", "COM1", "SerialName (COM1.../localhost:502)")
 	BaudPtr        = flag.Int("b", 9600, "Baud (1200/2400/4800/9600...)")
 	ReadTimeoutPtr = flag.Duration("rt", 2000000000, "ReadTimeout (1s/2s...)")
@@ -32,6 +34,7 @@ var (
 	DataBitsPtr    = flag.Int("d", 8, "Data bits: 5, 6, 7 or 8")
 	ParityPtr      = flag.String("p", "N", "Parity: N - None, E - Even, O - Odd")
 	StopBitsPtr    = flag.Int("sb", 1, "Stop bits: 1 or 2")
+	SuccessDevice  string
 )
 
 func init() {
@@ -65,7 +68,7 @@ func main() {
 	start := time.Now() // 获取当前时间
 	//Table Init
 	Table := tablewriter.NewWriter(os.Stdout)
-	header := []string{"ADDRESS", "ReadCoils 0x", "ReadDiscreteInputs 1x", "ReadInputRegisters 3x", "ReadHoldingRegisters 4x"}
+	header := []string{"ADDRESS", "ReadInputRegisters 3x", "ReadHoldingRegisters 4x"}
 	Table.SetHeader(header)
 	Table.SetAutoMergeCellsByColumnIndex([]int{0})
 	Table.SetRowLine(true)
@@ -124,7 +127,8 @@ func main() {
 		return
 	}
 
-	Table.SetFooter([]string{"", "", "", "Time-consuming", time.Since(start).String()})
+	Table.SetFooter([]string{"", "Time-consuming", time.Since(start).String()})
+	log.Infoln("SuccessDevice:", SuccessDevice)
 	Table.Render()
 }
 
@@ -134,11 +138,7 @@ func Scanner(client modbus.Client, SlaveId uint, Table *tablewriter.Table) {
 	var tabletwins []string
 	tableflag = append(tableflag, strconv.Itoa(int(SlaveId)))
 	tabletwins = append(tabletwins, strconv.Itoa(int(SlaveId)))
-	result, err := client.ReadCoils(uint16(*Address), uint16(*Quantity))
-	tableflag, tabletwins = SlaveError(result, "ReadCoils", SlaveId, err, tableflag, tabletwins)
-	result, err = client.ReadDiscreteInputs(uint16(*Address), uint16(*Quantity))
-	tableflag, tabletwins = SlaveError(result, "ReadDiscreteInputs", SlaveId, err, tableflag, tabletwins)
-	result, err = client.ReadInputRegisters(uint16(*Address), uint16(*Quantity))
+	result, err := client.ReadInputRegisters(uint16(*Address), uint16(*Quantity))
 	tableflag, tabletwins = SlaveError(result, "ReadInputRegisters", SlaveId, err, tableflag, tabletwins)
 	result, err = client.ReadHoldingRegisters(uint16(*Address), uint16(*Quantity))
 	tableflag, tabletwins = SlaveError(result, "ReadHoldingRegisters", SlaveId, err, tableflag, tabletwins)
@@ -152,16 +152,44 @@ func SlaveError(result []byte, modname string, SlaveId uint, err error, Table []
 		Table = append(Table, "Fail")
 		Twins = append(Twins, err.Error())
 	} else {
-		log.Infoln("SlaveId :", SlaveId, Success, "Result:", result)
+		SuccessDevice += strconv.FormatUint(uint64(SlaveId), 10) + ","
+		log.Infoln("SlaveId :", SlaveId, Success, "Result:", TransferData(result, *DataTypePtr))
 		Table = append(Table, "Success")
 		//TODO: result data to string.
-		Twins = append(Twins, string(result))
+		Twins = append(Twins, TransferData(result, *DataTypePtr))
 	}
 	return Table, Twins
 }
 
-func TransferData(value []byte) string {
-	data := float64(binary.BigEndian.Uint16(value))
-	sData := strconv.FormatUint(uint64(data), 10)
-	return sData
+func TransferData(value []byte, dataType string) string {
+	switch dataType {
+	case "uint16":
+		data := binary.BigEndian.Uint16(value)
+		return strconv.FormatUint(uint64(data), 10)
+	case "int16":
+		data := int16(binary.BigEndian.Uint16(value))
+		return strconv.FormatInt(int64(data), 10)
+	case "uint32":
+		data := binary.BigEndian.Uint32(value)
+		return strconv.FormatUint(uint64(data), 10)
+	case "int32":
+		data := int32(binary.BigEndian.Uint32(value))
+		return strconv.FormatInt(int64(data), 10)
+	case "uint64":
+		data := binary.BigEndian.Uint64(value)
+		return strconv.FormatUint(data, 10)
+	case "int64":
+		data := int32(binary.BigEndian.Uint64(value))
+		return strconv.FormatInt(int64(data), 10)
+	case "float32":
+		bits := binary.BigEndian.Uint32(value)
+		f := math.Float32frombits(bits)
+		return strconv.FormatFloat(float64(f), 'f', -1, 32)
+	case "float64":
+		bits := binary.BigEndian.Uint64(value)
+		f := math.Float64frombits(bits)
+		return strconv.FormatFloat(f, 'f', -1, 64)
+	default:
+		return ""
+	}
 }
